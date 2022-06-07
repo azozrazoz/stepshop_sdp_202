acdaa4379b7bfc5659df297cd0ed0b4674806cc5
azozrazoz <doshan.de@gmail.com>
2022-06-07 15:32:52 +0600

    test

--- stepshop/mainapp/management/commands/fill_db.py
+++ stepshop/mainapp/management/commands/fill_db.py
@@ -24,14 +24,19 @@ class Command(BaseCommand):
         products = load_from_json('products')
 
         for category in categories:
-            new_category = ProductCategory(**category)
+            _category = category.get('fields')
+            _category['id'] = category.get('pk')
+
+            new_category = ProductCategory(**_category)
             new_category.save()
 
         for product in products:
-            category_pk = product.get['category']
-            _category = ProductCategory.objects.get(pk=category_pk)
-            product['category'] = _category
-            new_product = Product(**product)
+            _product = product.get('fields')
+            category_id = _product.get('category')
+
+            _product['category'] = ProductCategory.objects.get(pk=category_id)
+
+            new_product = Product(**_product)
             new_product.save()
 
         User.objects.create_superuser('admin', 'admin@stepshop.kz', '123')
