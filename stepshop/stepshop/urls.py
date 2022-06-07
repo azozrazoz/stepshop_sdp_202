c54df16699f2fb603defe91fc04a6f05e0f046a2
azozrazoz <doshan.de@gmail.com>
2022-06-07 17:54:05 +0600

    add lesson_06 files

--- stepshop/stepshop/urls.py
+++ stepshop/stepshop/urls.py
@@ -26,6 +26,7 @@ urlpatterns = [
     path('about/', about, name='about'),
     path('contact/', contact, name='contacts'),
     path('products/', include('mainapp.urls', namespace='products')),
+    path('auth/', include('authapp.urls', namespace='auth'))
 ]
 
 if settings.DEBUG:
