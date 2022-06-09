3f5305f329461879f6ab3c9bee88147551a9b8f4
azozrazoz <doshan.de@gmail.com>
2022-06-09 17:47:19 +0600

    add lesson_08 files

--- stepshop/mainapp/views.py
+++ stepshop/mainapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from mainapp.models import Product, ProductCategory
 
 links_menu = [
@@ -9,11 +9,27 @@ links_menu = [
 ]
 
 
-def products(request):
+def products(request, pk=None):
     page_name = 'продукты'
 
-    products_ = Product.objects.all() # .filter(category__name__in=['Джинсы', 'Бобры']).order_by('price')
+    products_ = Product.objects.all()  # .filter(category__name__in=['Джинсы', 'Бобры']).order_by('price')
     categories_ = ProductCategory.objects.all()
+
+    if pk is not None:
+        if pk == 0:
+            products_ = Product.objects.all().order_by('price')
+            category = {'name': 'все'}
+        else:
+            category = get_object_or_404(ProductCategory, pk=pk)
+            products_ = Product.objects.filter(category__pk=pk).order_by('price')
+        context = {
+            'title': page_name,
+            'links_menu': links_menu,
+            'products': products_,
+            'categories': categories_,
+            'category': category,
+        }
+        return render(request, 'products.html', context)
 
     context = {
         'title': page_name,
